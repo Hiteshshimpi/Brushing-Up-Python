61af6bc26a647f116949af21e32f7b779f98c7dd
Hitesh Shimpi <hiteshshimpi@Hiteshs-MacBook-Air.local>
2026-01-31 21:39:40 +0530

    #Create three variables: age, height, and favorite_color and print type

---
+++ CodingExercise2.py
@@ -0,0 +1,6 @@
+#Create three variables: age, height, and favorite_color
+
+age, height, favorite_color= 25, 5.9,"Blue"
+print(f"Age: {age} |",f"Type:",type(age))
+print(f"Height: {age} |",f"Type:",type(height))
+print(f"Favorite Color: {age} |",f"Type:",type(favorite_color))
