63a3b68caea3773088b8e340533061ba293d69b7
Hitesh Shimpi <hiteshshimpi@Hiteshs-MacBook-Air.local>
2026-01-31 21:22:33 +0530

    Practice Python Basic Brushup

---
+++ CodingExercise1.py
@@ -0,0 +1,15 @@
+# create a variable name greeting and assign it the string
+# "Welcome to Python Programming".
+greeting ="Welcome to Python Programming"
+#print it
+print(greeting)
+
+#Modify the string to include your instructor name "Rahul!". For example,
+# "Welcome to Python Programming, [Instructor Name]!" and print the modified string.
+
+print("{} {}".format(greeting,"Hitesh"))
+
+print("#################alternate Way##################")
+# alternate way to do this
+
+print(f"{greeting}, Rahul!")
