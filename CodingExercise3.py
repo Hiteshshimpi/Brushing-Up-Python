566d3586c3b782f1d054d2e108c84cc55eace22c
Hitesh Shimpi <hiteshshimpi@Hiteshs-MacBook-Air.local>
2026-01-31 23:02:45 +0530

    #Create a list named fruits that contains below five different fruit names (strings).

---
+++ CodingExercise3.py
@@ -0,0 +1,6 @@
+#Create a list named fruits that contains below five different fruit names (strings).
+
+fruits =["apple", "banana", "cherry", "date", "elderberry"]
+print(f"First fruit: {fruits[0]}")
+print(f"Last fruit: {fruits[-1]}")
+print(f"Fruits from index 1 to 2: {fruits[1:3]}")
