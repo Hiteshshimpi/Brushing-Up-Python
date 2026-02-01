68af8f2fccb1113ed79e10d85d01d592a0537c5d
Hitesh Shimpi <hiteshshimpi@Hiteshs-MacBook-Air.local>
2026-02-01 10:33:30 +0530

    If Else and If block practice

---
+++ CodingExercise6.py
@@ -0,0 +1,13 @@
+# aWrite a program that assigns a greeting to a variable.
+# Use an if statement to check if the greeting is "Hello".
+# If it matches, print "Hello there!" and "How can I assist you today?".
+# If it does not match, print "Greetings!".
+# After the if-else block, print "Program has completed."
+
+greeting="Hello"
+if greeting in "Hello":
+    print("Hello there! \nHow can I assist you today?")
+else:
+    print("Greetings!")
+
+print("Program has completed.")
