5380c4aef38dc5ed8c84ab02f54dfc31df5f363e
Hitesh Shimpi <hiteshshimpi@Mac.lan>
2026-02-01 19:04:20 +0530

    The Average Calculator Function created

--- CodingExercise11.py
+++ CodingExercise11.py
@@ -2,3 +2,10 @@ def GreetUser(username):
     print("Hello " + username + "!\nWelcome to the Python Course")
 
 GreetUser("John")
+
+def CalculateAverage(num1, num2, num3):
+    sums = num1+num2+num3
+    avg = sums / 3
+    print(f"The average of {num1}, {num2}, and {num3} is {avg}\n")
+
+CalculateAverage(10,20,30)
