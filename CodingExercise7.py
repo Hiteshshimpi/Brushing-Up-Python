5bdf3e7e3f2b9dd40d6bdc89bd94d11486eb0bf3
Hitesh Shimpi <hiteshshimpi@Hiteshs-MacBook-Air.local>
2026-02-01 10:44:08 +0530

    Number Comparison

---
+++ CodingExercise7.py
@@ -0,0 +1,14 @@
+# Create a variable b and assign it a number 15.
+# Write an if statement to check if b is greater than 10.
+# If true, print "Number is greater than 10".
+# If false, print "Number is 10 or less".
+# Print "Comparison code is completed." at the end.
+a =15
+b = a
+
+if b > 10:
+    print("Number is greater than 10")
+else:
+    print("Number is 10 or less")
+
+print("Comparison code is completed.")
