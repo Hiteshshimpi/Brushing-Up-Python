e5b71eae091eb5ee08319da6f88a4a888b3c2810
Hitesh Shimpi <hiteshshimpi2310@gmail.com>
2026-02-13 20:51:20 +0530

    New coding exercise

---
+++ CodingExercise14.py
@@ -0,0 +1,24 @@
+# print o*o if n =3
+
+
+# n = int(input())
+#
+# for i in range(n):
+#     if i%2==0:
+#         print("0",end="")
+#     else:
+#         print("*",end="")
+# print("")
+
+# same question using while loop
+
+m=int(input())
+j =0
+while j<m:
+    if j % 2 ==0:
+        print("0",end="")
+    else:
+        print("*",end="")
+    j+=1
+
+print("")
