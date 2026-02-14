1f33dc789628b85f8aa9d67281da0551927ded4f
Hitesh Shimpi <hiteshshimpi2310@gmail.com>
2026-02-14 13:59:36 +0530

    New Pattern Questions print star patterns

--- patternProblems/pattern2.py
+++ patternProblems/pattern2.py
@@ -4,3 +4,18 @@ for i in range(n):
     for j in range(i+1):
         print(j+1, end=" ")
     print("")
+
+
+
+# another way to do this is using jump statements i.e break and continue
+
+print("Enter the number of rows ")
+a =int(input())
+for curRow in range(a): # this will run from 0->a-1
+    for curNum in range(a): # this will run from 0->a-1
+        if curNum<=curRow:
+            print(curNum+1, end=" ")
+        else:
+            #print(" ", end=" ") # instead of spaces we can add break statement to break the loop
+            break
+    print("")
