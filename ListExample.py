235d4597944bab2161041183eda94c1be516f3fd
Hitesh Shimpi <hiteshshimpi2310@gmail.com>
2026-02-16 14:39:49 +0530

    List slicing and comprehension

---
+++ ListExample.py
@@ -0,0 +1,18 @@
+# myList=[10,20,30,40,50,60]
+# print(myList)
+#
+# print(myList[0:3])
+#
+#
+# mylist1=[ele for ele in range(int(input()))]
+# print(mylist1)
+#
+# # and
+#
+# myList2 =[int(ele) for ele in input().split()]
+# print(myList2)
+
+# and
+
+myList3 = [ele for ele in int(input())]
+print(myList3)
