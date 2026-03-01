54a1fa9ada99da0f8e567b45e17e2ea2747fe751
Hitesh Shimpi <hiteshshimpi2310@gmail.com>
2026-03-01 20:14:05 +0530

    Roman numbers with conditions

--- romanNumbers.py
+++ romanNumbers.py
@@ -1,11 +1,15 @@
-dictRomanVals ={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-print(dictRomanVals.get('I'))
+d ={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
 
-romanVal="IV"
+s="III"
+n = len(s)
 sums=0
-for i in romanVal:
-    val = dictRomanVals.get(i)
-    print(val)
-    sums = sums + val
-
+i=0
+while i < n :
+    if  i < n-1 and d[s[i]] < d[s[i+1]] :
+        val = d[s[i + 1]] - d[s[i]]
+        sums += val
+        i += 2
+    else:
+        sums+=d[s[i]]
+        i+=1
 print(sums)
