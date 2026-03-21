9a2a0a52602a7266c2d12f48f48cbd5522a367b7
Hitesh Shimpi <hiteshshimpi2310@gmail.com>
2026-03-21 22:45:47 +0530

    chabges

--- MiddleIndexArray.py
+++ MiddleIndexArray.py
@@ -1,14 +1,14 @@
-nums=[2,3,-1,8,4]
-leftsum=0
-rightsum=0
+nums = [2, 3, -1, 8, 4]
+leftsum = 0
+rightsum = 0
 
 for i in range(len(nums)):
-    rightsum=rightsum+nums[i]
+    rightsum = rightsum + nums[i]
 
 for k in range(len(nums)):
-    if leftsum==rightsum - nums[k]:
+    if leftsum == rightsum - nums[k]:
         print(nums[k], k)
         break
     else:
-        leftsum=leftsum+nums[k]
-        rightsum=rightsum-nums[k]
+        leftsum = leftsum + nums[k]
+        rightsum = rightsum - nums[k]
