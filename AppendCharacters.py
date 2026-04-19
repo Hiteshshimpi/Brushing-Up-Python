53d8d2bb485b209670c237b6bafa527791f494e5
Hitesh Shimpi <hiteshshimpi2310@gmail.com>
2026-04-19 19:36:07 +0530

    AppendCharatcers

---
+++ AppendCharacters.py
@@ -0,0 +1,15 @@
+s = "coaching"
+t = "coding"
+count = 0
+
+i = 0
+j = 0
+
+while i < len(s) and j < len(t):
+    if s[i] == t[j]:
+        j += 1
+        i += 1
+    else:
+        i += 1
+
+print(len(t) - j)
